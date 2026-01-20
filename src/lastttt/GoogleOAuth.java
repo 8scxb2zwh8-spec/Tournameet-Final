bd4032abe250deae4f91fb0cfd66199adbc8f420
Hp <Hp@Kurt>
2026-01-20 21:31:24 +0800

    Merge origin/signup2mainmenu
    Conflicts:
    	build.xml
    	manifest.mf
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/lastttt/GoogleOAuth.java
+++ src/lastttt/GoogleOAuth.java
@@ -10,7 +10,7 @@ import java.net.URLEncoder;
 public class GoogleOAuth {
 
     //private static final String CLIENT_ID =
-            "824988333578-1estne2qmrtco8abio0ejqd2b37i3oer.apps.googleusercontent.com";
+            //"824988333578-1estne2qmrtco8abio0ejqd2b37i3oer.apps.googleusercontent.com";
 
     // ⚠️ Replace this with your real client secret
     //private static final String CLIENT_SECRET = "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";
